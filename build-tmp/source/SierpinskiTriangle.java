038b49e82bb620cf75b23e38b896593571f54cc1
unknown <jedelrio@697-334-Upbourn.sf.k12.internal>
2015-01-27 11:09:27 -0800

    Cool triangles

--- build-tmp/source/SierpinskiTriangle.java
+++ build-tmp/source/SierpinskiTriangle.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SierpinskiTriangle extends PApplet {
+
 public void setup()
 {  background(8,90,80);
 	size(500,500);
@@ -36,4 +52,13 @@ public void sierpinski(int x, int y, int len)
 		
 	}
 
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SierpinskiTriangle" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
